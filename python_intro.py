35cb66040aacd6ec4b5d89caed826ea9e5af19bb
Lee Spitzley <lspitzley@albany.edu>
2024-02-26 14:14:30 -0500

    python script from class

---
+++ python_intro.py
@@ -0,0 +1,18 @@
+"""
+This script is to show some basic Python syntax.
+
+This is a block comment. Nothing in here will be executed.
+"""
+
+# This is a single line comment. This is a comment.
+
+# This is a variable. It is a string.
+name = "Lee"
+
+# print this to the terminal
+print(name) 
+
+# imports
+import time
+
+print(time.asctime())
