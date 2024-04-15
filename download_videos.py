08203f3a9ca4d2c389692436c65d370e0e02abc8
Lee Spitzley <lspitzley@albany.edu>
2024-04-15 13:15:49 -0400

    outlined youtube video analysis system

---
+++ download_videos.py
@@ -0,0 +1,37 @@
+"""
+This script will select videos from a 
+pre-made list of video IDs (CNBC video metadata).
+
+It will download the videos, the transcripts,  
+the highest-quality thumbnail, and video-specific
+metadata. 
+"""
+
+#%% imports
+
+#%% functions
+
+def get_video_ids():
+    pass
+
+def get_metadata(video_id):
+    pass
+
+def get_transcripts(video_id):
+    pass
+
+def get_thumbnails(video_id):
+    pass
+
+def download_video(video_id):
+    """
+    This function uses the yt-dlp package to download
+    the video with the given video_id.
+    """
+    pass
+    
+
+#%% main
+
+if __name__ == '__main__':
+    pass
